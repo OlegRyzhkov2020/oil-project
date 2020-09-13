5003498c7bb9d3d00bbd23847e7b567ce1a8e96b
OlegRyzhkov2020 <61919810+OlegRyzhkov2020@users.noreply.github.com>
2020-09-12 20:10:48 -0500

    updating style and adding map zoom

--- js/donut_container.js
+++ js/donut_container.js
@@ -6,14 +6,14 @@ d3.csv("outputs/product_2019.csv", function(data) {
   showData(filteredData);
 });
 
-function showData(data) {
+function showData(filteredData) {
   let bodyHeight = 200;
   let bodyWidth = 400;
 
-  data = data.map(d => ({
-    country: d.country,
-    production: +d.production
-  }))
+  data = {}
+  for (var i=0; i<filteredData.length; i++) {
+    data[filteredData[i].country_code] = filteredData[i].production;
+  }
   console.log(data);
   // set the dimensions and margins of the graph
   var width = 450
@@ -27,21 +27,33 @@ function showData(data) {
   var svg = donut_container.append("svg")
       .attr("width", width)
       .attr("height", height)
-    .append("g")
+      .append("g")
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
   // Create dummy data
-  var data = {a: 9, b: 20, c:30, d:8, e:12}
+  // var data = {a: 9, b: 20, c:30, d:8, e:12}
 
   // set the color scale
   var color = d3.scaleOrdinal()
     .domain(data)
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", , "#6b486b", "#a05d56"])
+    .range(d3.schemeCategory10);
+    //.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#8a89a6"])
 
   // Compute the position of each group on the pie:
   var pie = d3.pie()
     .value(function(d) {return d.value; })
   var data_ready = pie(d3.entries(data))
+
+    // The arc generator
+  var arc = d3.arc()
+    .innerRadius(radius * 0.5)         // This is the size of the donut hole
+    .outerRadius(radius * 0.8)
+
+  // Another arc that won't be drawn. Just for labels positioning
+  var outerArc = d3.arc()
+    .innerRadius(radius * 0.9)
+    .outerRadius(radius * 1.2)
+
 
   // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
   svg
@@ -58,4 +70,38 @@ function showData(data) {
     .style("stroke-width", "2px")
     .style("opacity", 0.7);
 
+    // Add the polylines between chart and labels:
+  svg
+    .selectAll('allPolylines')
+    .data(data_ready)
+    .enter()
+    .append('polyline')
+      .attr("stroke", "black")
+      .style("fill", "none")
+      .attr("stroke-width", 1)
+      .attr('points', function(d) {
+        var posA = arc.centroid(d) // line insertion in the slice
+        var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
+        var posC = outerArc.centroid(d); // Label position = almost the same as posB
+        var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+        posC[0] = radius * 1 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+        return [posA, posB, posC]
+      })
+      // Add the polylines between chart and labels:
+  svg
+    .selectAll('allLabels')
+    .data(data_ready)
+    .enter()
+    .append('text')
+      .text( function(d) { console.log(d.data.key) ; return d.data.key } )
+      .attr('transform', function(d) {
+          var pos = outerArc.centroid(d);
+          var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+          pos[0] = radius * 1.0 * (midangle < Math.PI ? 1 : -1);
+          return 'translate(' + pos + ')';
+      })
+      .style('text-anchor', function(d) {
+          var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+          return (midangle < Math.PI ? 'start' : 'end')
+      })
   }
--- js/world_map.js
+++ js/world_map.js
@@ -11,26 +11,38 @@ function mapData(mapInfo) {
 
   let projection = d3.geoNaturalEarth1()
     .scale(150)
-    .translate([bodyWidth/2, bodyHeight/2]);
+    .translate([bodyWidth/2, bodyHeight/2])
+    .rotate([-150,0]);
 
   let path = d3.geoPath()
     .projection(projection);
 
   var map_svg = map_container.append("svg")
     .attr("width", bodyWidth)
-    .attr("height", bodyHeight);
-  var g = map_svg.append("g");
+    .attr("height", bodyHeight)
+    .call(d3.zoom().on("zoom", function () {
+      map_svg.attr("transform", d3.event.transform)
+    }))
+    .append("g");
 
   map_svg.selectAll("path")
     .data(mapInfo.features)
     .enter().append("path")
     .attr("d", d => path(d))
     .attr("stroke", "black")
-    .attr("fill", "none")
+    .attr("fill", "none");
 
-//  var map_svg = map_container.append("svg")
-//      .attr("width", bodyWidth)
-//      .attr("height", bodyHeight);
-//  var g = map_svg.append("g");
+    // load and display the World
+    //d3.json("json/world-110m2.json", function(error, topology) {
+      //g.selectAll("path")
+      //.data(topojson.object(topology, topology.objects.countries)
+        //  .geometries)
+      //.enter()
+      //.append("path")
+      //.attr("d", path)
+    //});
+
+    // zoom and pan
+
 
 };
