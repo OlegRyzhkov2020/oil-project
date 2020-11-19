b8bb7ed1e5bdcf0bae236081c319656cef53a68a
Zuzana Svorenova <zuzana.svorenova@gmail.com>
2020-11-19 17:08:09 -0600

    updated datasets and flow working

--- export_import/js/export_import.js
+++ export_import/js/export_import.js
@@ -41,20 +41,24 @@ var curve = function(context) {
     return custom;
   }
 
+  var projection = d3.geoKavrayskiy7()
+    // .scale(200)
+    .rotate([-220, -5])
+    .translate([width/2, height/2])
+    .precision(0.1);
+    
   var line=d3.line()
   .x(function(d) {
+    //   return projection ([+d.lon, +d.lat])[0];
     return projection (d)[0];
   })
   .y(function(d) {
+    // return projection ([+d.lon, +Sd.lat])[1];
     return projection (d)[1];
   })
   .curve(curve)
 
-var projection = d3.geoKavrayskiy7()
-    // .scale(200)
-    .rotate([-205, -5])
-    .translate([width/2, height/2])
-    // .precision(0.1);
+
 
 var path = d3.geoPath().projection(projection);
 
@@ -85,35 +89,6 @@ var buttonsText = g2.selectAll(".flowLabel")
 			.text(function(d) { return d; })
 			.attr("class","commodityLabel")
             .on("click",function(d) { updateFlow(d); });
-
-/// v1: line animation source:
-// http://bl.ocks.org/erikhazzard/6201948
-// https://stackoverflow.com/questions/18165533/how-to-draw-a-line-link-between-two-points-on-a-d3-map-based-on-latitude-lon
-
-// var lineTransition = function lineTransition(path) {
-//     path.transition()
-//     //NOTE: Change this number (in ms) to make lines draw faster or slower
-//     .duration(5500)
-//     .attrTween("stroke-dasharray", tweenDash)
-//     // .each("end", function(d,i) { 
-//         ////Uncomment following line to re-transition
-//         // d3.select(this).call(transition); 
-                  
-//         //We might want to do stuff when the line reaches the target,
-//         //  like start the pulsating or add a new point or tell the
-//         //  NSA to listen to this guy's phone calls
-//         //doStuffWhenLineFinishes(d,i);
-//         // });
-// };
-// var tweenDash = function tweenDash() {
-//     //This function is used to animate the dash-array property, which is a
-//     //  nice hack that gives us animation along some arbitrary path (in this
-//     //  case, makes it look like a line is being drawn from point A to B)
-//     var len = this.getTotalLength(),
-//         interpolate = d3.interpolateString("0," + len, len + "," + len);
-
-//     return function(t) { return interpolate(t); };
-// };
 
 function getUSA(feature) {
     return (feature.properties["Reporter ISO"] === "USA" &&
@@ -150,37 +125,29 @@ d3.json("./data/countries-110m.json",function(error,world) {
         .attr("class", "land")
         .attr("d", path);
 
-    // enableRotation()
 });}
 
+// ./data/oil_country_flows_${flow}s.geojson
 function drawFlow(flow) {
-    d3.json(`./data/oil_country_flows_${flow}s.geojson`, function(error, dataset) {
+    d3.json(`${flow}.geojson`, function(error, dataset) {
         console.log(getColor(flow))
-        console.log(dataset);
+        // console.log(dataset);
         var features = dataset.features;
         console.log(features);
-        
-        var usaTop10 = features.filter(getUSA)
-                            .sort((a, b) => b.properties["Qty"] - a.properties["Qty"])
-                            .slice(0,10);
-        
-        console.log(usaTop10);
-
+    
         // inspired by: http://bl.ocks.org/Andrew-Reid/35d89fbcfbcfe9e819908ea77fc5bef6
-        var maxVolume = d3.max(usaTop10, function(d) {  return d.properties["Qty"]; });
-        // console.log(maxVolume);
+        var maxVolume = d3.max(features, function(d) {  return d.properties["volume"]; });
+        console.log(maxVolume);
         
-        usaTop10.forEach( function(d,i) {
+        features.forEach( function(d,i) {
             var flow1 = g
                 .append("path")
                 .attr("d", line(d.geometry.coordinates))
                 .attr("class", "flow-line")
                 .style("stroke", getColor(flow)) 
-                .attr("stroke-opacity", Math.sqrt(d.properties["Qty"] / maxVolume) )
+                .attr("stroke-opacity", Math.sqrt((+d.properties["volume"] / maxVolume)*2) )
                 .attr("stroke-width", 5);
-            // console.log(d.properties["Qty"])
-            // console.log(Math.sqrt(d.properties["Qty"]))
-            // console.log(Math.sqrt(d.properties["Qty"] / maxVolume));
+            
             var totalLength = flow1.node().getTotalLength() +10;
 
             flow1
@@ -192,83 +159,9 @@ function drawFlow(flow) {
             .attr("stroke-dashoffset", 0);
         });
 
-        // // v1 inspired by: https://stackoverflow.com/questions/56562231/line-on-d3-map-not-forming-a-curve
-        // var fauxArcPaths = svg.selectAll(null)
-        //     .data(usaTop10)
-        //     .enter()
-        //     .append("path")
-        //     .datum(function(d) {
-        //         return d.geometry.coordinates;
-        //         // return [d.source,d.destination];
-        //     })
-        //     .attr("class", "flow-line")
-        //     .attr("d",line)
-        //     .style("stroke", getColor(flow)) 
-        //     .style("stroke-width",1)
-        //     .call(lineTransition);
     });
-    // enableRotation();
+    
 };
-
-// d3.json(`./data/oil_country_flows_exports.geojson`, function(error, expoJson) {
-//     console.log(expoJson);
-//     var features = expoJson.features;
-//     console.log(features);
-    
-//     var usa10Export = features.filter(getUSA)
-//                         .sort((a, b) => b.properties["Qty"] - a.properties["Qty"])
-//                         .slice(0,10);
-    
-//     console.log(usa10Export);
-
-//     // L.geoJSON(usa10Export).addTo(mapLeaflet);
-
-//     var fauxArcPaths = svg.selectAll(null)
-//     .data(usa10Export)
-//     .enter()
-//     .append("path")
-//     .datum(function(d) {
-//         return d.geometry.coordinates;
-//         // return [d.source,d.destination];
-//     })
-//     .attr("d",line)
-//     .style("stroke","red")
-//     .style("stroke-width",1)
-//     .call(lineTransition);
-// });
-
-// d3.json("./data/oil_country_flows_imports.geojson", function(error, importJson) {
-//     console.log(importJson);
-//     var featuresImport = importJson.features;
-//     console.log(featuresImport);
-    
-//     var usa10Import = featuresImport.filter(getUSA)
-//                         .sort((a, b) => b.properties["Qty"] - a.properties["Qty"])
-//                         .slice(0,10);
-    
-//     console.log(usa10Import);
-
-//     // L.geoJSON(usa10Import).addTo(mapLeaflet);
-//     console.log(usa10Import[2].geometry)
-
-
-//     ///////////////////////////////////////////////////////
-//     //////////////////////////////////////////////////////
-//     // source: https://stackoverflow.com/questions/56562231/line-on-d3-map-not-forming-a-curve 
-    
-//     var fauxArcPaths = svg.selectAll(null)
-//         .data(usa10Import)
-//         .enter()
-//         .append("path")
-//         .datum(function(d) {
-//             return d.geometry.coordinates;
-//            })
-//         .attr("d",line)
-//         .style("stroke","black")
-//         .style("stroke-width",1)
-//         .call(lineTransition);
-
-// });
 
 drawGlobe();
 
